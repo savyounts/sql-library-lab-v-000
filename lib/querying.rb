ac02788a5d95cbc5e2ed8da783ccf50e78586eb0
Sav Younts <savyounts@gmail.com>
2018-08-22 19:44:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.title, subgenre.name FROM authors
+  "SELECT authors.name, series.title, subgenres.name FROM authors
   JOIN series ON series.author_id = authors.id
   JOIN subgenres ON subgenres.id = series.subgenre_id"
 end
