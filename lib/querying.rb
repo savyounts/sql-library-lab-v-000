baa845641175ec901e0badbff6a1b9715fa3e309
Sav Younts <savyounts@gmail.com>
2018-08-22 19:50:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series 
+  "SELECT series.title FROM series
   JOIN characters ON characters.series_id = series.id
   GROUP BY series.title
   COUNT(characters.species) WHERE characters.species = 'human' "
