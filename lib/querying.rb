d4a31daa1d49a3803a94a43eccbf9bb93db1b4ce
Sav Younts <savyounts@gmail.com>
2018-08-22 20:05:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,6 +36,5 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name FROM characters
   JOIN character_books ON character_books.character_id = characters.id
-  WHERE character_books.character_id = characters.id
-  ORDER BY COUNT(book_id) DESC"
+  WHERE character_books.character_id = characters.id"
 end
