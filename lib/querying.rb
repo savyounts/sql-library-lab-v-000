8001e3cb2a29884d02049b0849ff2641ab7a20e3
Sav Younts <savyounts@gmail.com>
2018-08-22 19:55:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters ON characters.series_id = series.id
   GROUP BY series.id
-  COUNT(characters.species)
+  COUNT(*)
   WHERE characters.species = 'Human'
 
   ORDER BY COUNT(characters.species) DESC
