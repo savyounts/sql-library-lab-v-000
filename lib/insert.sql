b53ba333a91c140cb2a8926b142c37a49ccded85
Sav Younts <savyounts@gmail.com>
2018-08-22 19:14:59 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+/*series*/
+INSERT INTO series (title, author_id, sub_genre_id) VALUES ("title", 1, 1);
+INSERT INTO series (title, author_id, sub_genre_id) VALUES ("title", 1, 1);
+
+/*sub-genre*/
+INSERT INTO sub_genre (name) VALUES ("name");
+INSERT INTO sub_genre (name) VALUES ("name");
+
+/*authors*/
+INSERT INTO authors (name) VALUES ("name");
+INSERT INTO authors (name) VALUES ("name");
+
+/*books*/
+INSERT INTO books (title, year, series_id) VALUES ("title", 1990, 1);
+INSERT INTO books (title, year, series_id) VALUES ("title", 1990, 1);
+INSERT INTO books (title, year, series_id) VALUES ("title", 1990, 1);
+INSERT INTO books (title, year, series_id) VALUES ("title", 1990, 2);
+INSERT INTO books (title, year, series_id) VALUES ("title", 1990, 2);
+INSERT INTO books (title, year, series_id) VALUES ("title", 1990, 2);
+
+/*characters*/
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("name", "motto", "species", 1, 1);
